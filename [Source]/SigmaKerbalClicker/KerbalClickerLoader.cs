04c363d89c371bf8fa371f4de8e611fc44eac0d3
Sigma88 <sigmamodexpansions@outlook.com>
2020-02-24 17:38:20 +0100

    better loader

--- [Source]/SigmaKerbalClicker/KerbalClickerLoader.cs
+++ [Source]/SigmaKerbalClicker/KerbalClickerLoader.cs
@@ -6,13 +6,13 @@ namespace SigmaKerbalClickerPlugin
     [KSPAddon(KSPAddon.Startup.MainMenu, false)]
     internal class KerbalClickerLoader : MonoBehaviour
     {
-        bool skip = false;
-
         void Start()
         {
-            if (skip) return;
-            skip = true;
+            StartCoroutine(CallbackUtil.DelayedCallback(1, AddKerbalClicker));
+        }
 
+        void AddKerbalClicker()
+        {
             GameObject[] scenes = FindObjectOfType<MainMenu>().envLogic.areas;
 
             for (int i = 0; i < 2; i++)
@@ -23,23 +23,51 @@ namespace SigmaKerbalClickerPlugin
                 {
                     GameObject kerbal = scenes[i].GetChild("Kerbals").transform.GetChild(j).gameObject;
 
-                    GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-                    capsule.transform.SetParent(kerbal.GetChild("joints01").transform, true);
-                    capsule.transform.localPosition = new Vector3(0, 0.42f, -0.025f);
-                    capsule.transform.localScale = new Vector3(0.25f, 0.42f, 0.35f);
-                    capsule.transform.localRotation = Quaternion.identity;
-                    DestroyImmediate(capsule.GetComponent<Renderer>());
+                    GameObject capsule = kerbal.GetChild("joints01_capsule_collider");
 
-                    // Kerbal Clicker
-                    KerbalClicker kc = capsule.AddComponent<KerbalClicker>();
+                    if (capsule == null)
+                    {
+                        capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                        capsule.name = "joints01_capsule_collider";
+                        capsule.layer = 15;
+                        capsule.transform.SetParent(kerbal.GetChild("joints01").transform, true);
+                        capsule.transform.localPosition = new Vector3(0, 0.42f, -0.025f);
+                        capsule.transform.localScale = new Vector3(0.25f, 0.42f, 0.35f);
+                        capsule.transform.localRotation = Quaternion.identity;
+                        DestroyImmediate(capsule.GetComponent<Renderer>());
+                    }
 
-                    // Mun Scene
-                    kc.mrka = kerbal.GetComponent<MenuRandomKerbalAnims>();
+                    GameObject helmet = kerbal.GetChild("helmet01") ?? kerbal.GetChild("mesh_female_kerbalAstronaut01_helmet01");
+                    GameObject sphere = kerbal.GetChild("bn_helmet01_sphere_collider");
+
+                    if (helmet?.activeSelf == true && sphere == null)
+                    {
+                        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        sphere.name = "bn_helmet01_sphere_collider";
+                        sphere.layer = 15;
+                        sphere.transform.SetParent(kerbal.GetChild("bn_helmet01").transform, true);
+                        sphere.transform.localPosition = new Vector3(-0.25f, 0.05f, 0);
+                        sphere.transform.localScale = new Vector3(0.58f, 0.58f, 0.58f);
+                        sphere.transform.localRotation = Quaternion.identity;
+                        DestroyImmediate(sphere.GetComponent<Renderer>());
+                    }
+
+                    // Kerbal Clicker
+                    KerbalClicker kc = capsule.AddOrGetComponent<KerbalClicker>();
+
+                    // Mun Scene
+                    kc.mrka = kerbal.GetComponent<MenuRandomKerbalAnims>();
                     kc.animation = kerbal.GetComponent<Animation>();
 
-                    // OrbitScene
-                    kc.animator = kerbal.GetComponent<Animator>();
+                    // OrbitScene
+                    kc.animator = kerbal.GetComponent<Animator>();
                     kc.bobber = kerbal.GetComponent<Bobber>();
+
+                    // Helmet Clicker
+                    if (sphere != null)
+                    {
+                        sphere.AddOrGetComponent<HelmetClicker>().kc = kc;
+                    }
                 }
             }
         }
